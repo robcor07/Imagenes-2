9c32fef1ab1b5154cbdc9b696f50fb796f07aebf
robcor07 <88813815+robcor07@users.noreply.github.com>
2023-11-22 12:28:21 -0600

    Dibujar_Manipulador.py

--- Dibujar_Manipulador.py
+++ Dibujar_Manipulador.py
@@ -11,6 +11,8 @@ def dibujar_manipulador(q):
     theta_2 = q[1]
     theta_3 = q[2]
 
+    
+
     t01 = np.array([a_1 * np.cos(theta_1), a_1 * np.sin(theta_1)])
     t02 = np.array([a_2*np.cos(theta_1 + theta_2) + a_1*np.cos(theta_1),
                     a_2*np.sin(theta_1 + theta_2) + a_1*np.sin(theta_1)])
@@ -20,6 +22,8 @@ def dibujar_manipulador(q):
     plt.plot([0, t01[0]], [0, t01[1]])
     plt.plot([t01[0], t02[0]], [t01[1], t02[1]])
     plt.plot([t02[0], t03[0]], [t02[1], t03[1]])
+
+    plt.scatter(0.4, 0.4, marker='o', s=100)
 
     plt.scatter(0, 0, marker='o', s=100)
     plt.scatter(t01[0], t01[1], marker='o', s=100)
